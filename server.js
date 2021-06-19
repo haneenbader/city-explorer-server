f601b7bed17edd5dca7c667751e91d9c4c7f6141
haneenbader <haneenalwatan993@gmail.com>
2021-06-19 13:30:59 +0300

    lab07

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 const cors = require('cors');
+const weatherData = require ('./data/weather.json')
 
-app.use(cors()) // after you initialize your express app instance
 
 require('dotenv').config();
 const PORT = process.env.PORT
 // a server endpoint 
+
+app.use(cors()) // after you initialize your express app instance
+
 app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
+function  (req, res) { // callback function of what we should do with our request
   res.send('Hello World') // our endpoint function response
-})
- 
-app.listen(PORT) // kick start the express server to work
+});
+
+ app.get('/weather', function (req,res) {
+   const responseData = weatherData.data.map(element => new Weather (element));
+    res.json(responseData)
+});
+
+// modal : data how data look 
+class Weather {
+  constructor(data){
+    this.description = data.weather.description;
+    this.date = data.valid_date;
+  }
+}
+
+// kick start the express server to work
+app.listen(PORT , ()=>{
+  console.log(`server started on ${PORT}`)
+});
